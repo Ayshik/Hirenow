3bf7f7710a32eb8ebe84e4be587400cc881d39a5
MD WALI MOSNAD AYSHIK <61822224+Ayshik@users.noreply.github.com>
2021-09-19 03:31:42 +0600

    ok

--- Controller/assign.php
+++ Controller/assign.php
@@ -3,9 +3,7 @@ session_start();
   require_once '../Models/db_connect2.php';
  
 $query ="UPDATE `hirereq` SET `employee`='$_GET[id]',status='ongoing' WHERE `sl`='$_GET[sl]'";
-$query2 ="UPDATE `employee` SET `status`='Inactive' WHERE `username`='$_GET[id]'";
   execute($query);
-   execute($query2);
 header("Location:../View/Modarator/userrequest.php");
 
 
--- Hirenow/View/Modarator/footer/footer.php
+++
@@ -1,8 +0,0 @@
-
-
-<center><footer>
-  <p style="color:green; font-weight: bold;">Copy right 2021 Aiub hirenow</p>
-</footer>
-
-</body>
-</html>
